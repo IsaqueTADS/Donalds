e56d82ef6efbf4a9119d5d2973f9f5f72122117c
IsaqueTADS <isaqueofc18@gmail.com>
2025-02-23 03:06:26 -0300

    feat: add initial restaurant page and menu page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+
 const HomePage = () => {
-  return <h1>HELLO MY VIDA</h1>;
+  return (
+    <>
+      <h1
+        style={{
+          color: "green",
+          fontSize: "3.5rem",
+        }}
+      >
+        Eu gosto mt de react
+      </h1>
+
+      <p>
+        afinal eu posso fazer o que eu quero da forma que eu quero juntando o
+        que eu mais gosto
+      </p>
+
+      <h2>{4 * 8}</h2>
+    </>
+  );
 };
 
 export default HomePage;
