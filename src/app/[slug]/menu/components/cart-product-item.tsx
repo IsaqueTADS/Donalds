205b0377bfb7fe1e6e8465ea2eb9767035d56aff
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-25 19:14:22 -0300

    feat: add initial finish order dialog components

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -5,63 +5,64 @@ import { useContext } from "react";
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/helpers/format-currency";
 
-import {CartContext, CartProduct} from "../contexts/cart"
+import { CartContext, CartProduct } from "../contexts/cart";
 
-
-
-interface CartItemProps { 
-  product: CartProduct; 
-
+interface CartItemProps {
+  product: CartProduct;
 }
 
-const CartProductItem = ({product}: CartItemProps) => {
-
-  const {decreaseProductQuantity, increeseProductQuantity,removeProduct} = useContext(CartContext)
+const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity, increeseProductQuantity, removeProduct } =
+    useContext(CartContext);
 
   return (
+    <div className="flex items-center justify-between">
+      {/* ESQUERDA */}
 
+      <div className="flex items-center gap-3">
+        <div className="relative h-20 w-20 rounded-xl bg-gray-200">
+          <Image src={product.imageUrl} alt={product.name} fill />
+        </div>
+        <div className="space-y-1">
+          <p className="max-w-[90%] truncate text-ellipsis text-xs">
+            {product.name}
+          </p>
+          <p className="text-sm font-semibold">
+            {formatCurrency(product.price)}
+          </p>
+          {/* QUANTIDADE */}
 
-      <div className="flex items-center justify-between">
-
-        {/* ESQUERDA */}
-
-          <div className="flex items-center gap-3">
-            <div className="relative h-20 w-20 bg-gray-200 rounded-xl">
-              <Image src={product.imageUrl} alt={product.name} fill/>
-            </div>
-            <div className="space-y-1">
-              <p className=" max-w-[90%] text-xs truncate text-ellipsis">{product.name}</p>
-              <p className="text-sm font-semibold">{formatCurrency(product.price)}</p>
-              {/* QUANTIDADE */}
-            
-              <div className="flex items-center gap-1 text-center">
-                <Button className="h-7 w-7 rounded-lg" variant="outline" onClick={() => decreaseProductQuantity(product.id)}>
-                  <ChevronLeftIcon size={14} />
-                </Button>
-                <p className="w-7 text-xs">{product.quantity}</p>
-                <Button className="h-7 w-7 rounded-lg" variant="destructive" onClick={() => increeseProductQuantity(product.id)}>
-                  <ChevronRightIcon  />
-                </Button>
-            
-              </div>
-            </div>
+          <div className="flex items-center gap-1 text-center">
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="outline"
+              onClick={() => decreaseProductQuantity(product.id)}
+            >
+              <ChevronLeftIcon size={14} />
+            </Button>
+            <p className="w-7 text-xs">{product.quantity}</p>
+            <Button
+              className="h-7 w-7 rounded-lg"
+              variant="destructive"
+              onClick={() => increeseProductQuantity(product.id)}
+            >
+              <ChevronRightIcon />
+            </Button>
           </div>
-
-          {/* BOTÃO DE DELETAR */}
-
-          <Button className="h-7 w-7 rounded-lg" variant ="outline" onClick={ () => removeProduct(product.id)} >
-
-            <TrashIcon  />
-
-          </Button>
-
+        </div>
       </div>
 
+      {/* BOTÃO DE DELETAR */}
 
-    );
-}
- 
+      <Button
+        className="h-7 w-7 rounded-lg"
+        variant="outline"
+        onClick={() => removeProduct(product.id)}
+      >
+        <TrashIcon />
+      </Button>
+    </div>
+  );
+};
+
 export default CartProductItem;
-
-
-
