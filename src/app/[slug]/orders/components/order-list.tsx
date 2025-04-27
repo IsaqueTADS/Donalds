fb2eccdadc38905536facecb49ccf0d8a181c0dd
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-27 04:05:19 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/[slug]/orders/components/order-list.tsx
+++ src/app/[slug]/orders/components/order-list.tsx
@@ -4,7 +4,7 @@
 import {OrderStatus, Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
-import { useParams, useRouter, useSearchParams } from "next/navigation";
+import {  useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
