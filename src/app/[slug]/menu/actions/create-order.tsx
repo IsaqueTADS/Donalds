da623216172b66e1c6a2c71bd005a28c0b13801a
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-27 03:36:59 -0300

    feat: ensure cache is revalidate when creating an order

--- src/app/[slug]/menu/actions/create-order.tsx
+++ src/app/[slug]/menu/actions/create-order.tsx
@@ -1,7 +1,7 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
@@ -58,7 +58,7 @@ export const createOrder = async (input: CreateOrderInput) => {
     },
   });
 
-  // revalidatePath(`/${input.slug}/orders`);
+  revalidatePath(`/${input.slug}/orders`);
 
    
 
