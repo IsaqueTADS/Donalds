14be4ff5c4a7eec0f42a52fcfedc34ee56c86ac7
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-27 02:35:04 -0300

    feat: add orders page

--- src/app/[slug]/menu/actions/create-order.tsx
+++ src/app/[slug]/menu/actions/create-order.tsx
@@ -1,7 +1,8 @@
 "use server";
 
 import { ConsumptionMethod } from "@prisma/client";
-import { revalidatePath } from "next/cache";
+// import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 import { db } from "@/lib/prisma";
 
@@ -57,9 +58,14 @@ export const createOrder = async (input: CreateOrderInput) => {
     },
   });
 
-  revalidatePath(`/${input.slug}/orders`);
-  // redirect(
-  //   `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
-  // );
+  // revalidatePath(`/${input.slug}/orders`);
+
+   
+
+   
+
+  redirect(
+    `/${input.slug}/orders?cpf=${removeCpfPunctuation(input.customerCpf)}`,
+  );
   return order;
 };
