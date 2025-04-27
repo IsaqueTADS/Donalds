b4193ab7d8b3a3a50d6f509c7ea268a924c21c55
IsaqueTADS <isaqueofc18@gmail.com>
2025-04-27 04:07:40 -0300

    feat: ensure all orders and back buttons work as expected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,5 @@
-
 const HomePage = () => {
-  return (
-    <>
-      <h1
-        style={{
-          color: "green",
-          fontSize: "3.5rem",
-        }}
-      >
-        Eu gosto mt de react
-      </h1>
-
-      <p>
-        afinal eu posso fazer o que eu quero da forma que eu quero juntando o
-        que eu mais gosto
-      </p>
-
-      <h2>{4 * 8}</h2>
-    </>
-  );
+  return <h1>NADA DEMAIS AQUI, CURIOSO</h1>;
 };
 
 export default HomePage;
